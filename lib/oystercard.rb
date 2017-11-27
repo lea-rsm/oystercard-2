7498ac24a3bb104b1a6210c26d8991a4520a149d
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-27 17:38:51 +0000

    Step 6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,19 @@
 class Oystercard
 	attr_reader :balance
-	
+	DEFAULT_LIMIT = 90
+
 	def initialize(balance = 0)
 		@balance = balance
+		@balance_limit = 90
 	end
 
 	def add_money(quantity)
+		raise 'The balance limit is 90 pounds' if balance_full?(quantity)
 		@balance += quantity
+	end
+
+	private
+	def balance_full?(quantity)
+		(@balance + quantity) > DEFAULT_LIMIT
 	end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,4 @@
-require "oystercard"
+require 'oystercard.rb'
 
 describe Oystercard do
 
@@ -8,5 +8,9 @@ describe Oystercard do
 
 	it 'can be loaded with money' do
 		expect{ subject.add_money 5 }.to change{ subject.balance }.by 5
+	end
+
+	it 'has a balance limit of 90 Pounds' do
+		expect{ subject.add_money 91 }.to raise_error 'The balance limit is 90 pounds'
 	end
 end
