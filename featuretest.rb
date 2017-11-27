cdde9f9184253ed3321fd462693efdbf0ab0dc7f
kkumshayev <kumshkesh@gmail.com>
2017-11-27 17:25:45 +0000

    part 6 failing feature test

--- featuretest.rb
+++ featuretest.rb
@@ -1,10 +1,16 @@
 require './lib/oystercard'
 
-# new oyster card
+# parts 4-5
+# # new oyster card
 p oyster = Oystercard.new
-# should display a balance of 0
-p oyster.balance
-# add some money
-p oyster.add_money(5)
-# balance should now return 5
-p oyster.balance
+# # should display a balance of 0
+# p oyster.balance
+# # add some money
+# p oyster.add_money(5)
+# # balance should now return 5
+# p oyster.balance
+
+# part 6
+p 'expect an error message when adding 91'
+p 'pounds to the oyster card because the limit is 90'
+p oyster.add_money(91)
