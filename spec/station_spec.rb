eecdb98dd563cc5f5e8113a17aaf28e9a7ac7ffb
kkumshayev <kumshkesh@gmail.com>
2017-11-29 17:15:01 +0000

    passing initialization unit tests for station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,6 +1,22 @@
 require 'station'
 
 describe Station do
+	# let(:station) { double(:station, name: 'name', zone: 'zone') }
+
+    let(:name) { double(:name) }
+    let(:zone) { double(:zone) }
+
+	subject(:station) { described_class.new(name,zone) }
+
 	it { is_expected.to respond_to(:name) }
 	it { is_expected.to respond_to(:zone) }
+
+	describe '#initialize' do
+		it 'initializes with a name' do
+			expect(subject.name).to eq name
+		end
+		it 'initializes with a zone' do
+			expect(subject.zone).to eq zone
+		end
+	end
 end
