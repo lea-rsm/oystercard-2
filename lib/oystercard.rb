ad354489023f401d08e269a2bc6bbdf79e8be093
Lan Pham <lanpham312@gmail.com>
2017-11-29 16:24:10 +0000

    passing feature test for creating journey log

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,49 +1,48 @@
 class Oystercard
-	attr_reader :balance, :entry_station, :exit_station, :log
-	BALANCE_LIMIT = 90
-	MINIMUM_BALANCE = 0
-	MINIMUM_FARE = 2
+  attr_reader :balance, :entry_station, :exit_station, :log
+  BALANCE_LIMIT = 90
+  MINIMUM_BALANCE = 0
+  MINIMUM_FARE = 2
 
-	def initialize
-		@balance = 0
-		@entry_station = nil
-		@exit_station = nil
-		@log = []
-	end
+  def initialize
+    @balance = 0
+    @entry_station = nil
+    @exit_station = nil
+    @log = []
+  end
 
-	def add_money(amount)
-		raise "The balance limit is #{BALANCE_LIMIT} pounds" if over_limit?(amount)
-		@balance += amount
-	end
+  def add_money(amount)
+    raise "The balance limit is #{BALANCE_LIMIT} pounds" if over_limit?(amount)
+    @balance += amount
+  end
 
-	def touch_in(station)
-		raise "Insufficient Funds!" if insufficient_funds?
-		@entry_station = station
-		@in_journey = true
-	end
+  def touch_in(station)
+    raise 'Insufficient Funds!' if insufficient_funds?
+    @entry_station = station
+  end
 
-	def touch_out(station)
-		deduct(MINIMUM_FARE)
-		@entry_station = nil
-		@exit_station = station
-		@in_journey = false
-	end
+  def touch_out(station)
+    deduct(MINIMUM_FARE)
+    @exit_station = station
+    @log << { @entry_station => @exit_station }
+    @entry_station = nil
+  end
 
-	def in_journey?
-		!@entry_station.nil?
-	end
+  def in_journey?
+    !@entry_station.nil?
+  end
 
-	private
+  private
 
-	def deduct(amount)
-		@balance -= amount
-	end
+  def deduct(amount)
+    @balance -= amount
+  end
 
-	def over_limit?(amount)
-		(@balance + amount) > BALANCE_LIMIT
-	end
+  def over_limit?(amount)
+    (@balance + amount) > BALANCE_LIMIT
+  end
 
-	def insufficient_funds?
-		@balance < MINIMUM_FARE
-	end
+  def insufficient_funds?
+    @balance < MINIMUM_FARE
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,91 +1,88 @@
 require 'oystercard.rb'
 
 describe Oystercard do
-	it { is_expected.to respond_to(:add_money).with(1).argument }
-	it { is_expected.to respond_to(:touch_in).with(1).argument }
-	it { is_expected.to respond_to(:touch_out).with(1).argument }
-	it { is_expected.to respond_to(:log)}
+  it { is_expected.to respond_to(:add_money).with(1).argument }
+  it { is_expected.to respond_to(:touch_in).with(1).argument }
+  it { is_expected.to respond_to(:touch_out).with(1).argument }
+  it { is_expected.to respond_to(:log) }
 
-	let(:entry_station) { double(:entry_station) }
-	let(:exit_station) { double(:exit_station) }
+  let(:entry_station) { double(:entry_station) }
+  let(:exit_station) { double(:exit_station) }
 
-	ADD_MONEY = 60
-	TEST_DEDUCT_MONEY = 2
+  ADD_MONEY = 60
+  TEST_DEDUCT_MONEY = 2
 
-	describe "#initalize" do
-		it "should check that the card has an empty list of journey by default" do
-			expect(subject.log).to be_empty
-		end
-	end
+  describe '#initalize' do
+    it 'should check that the card has an empty list of journey by default' do
+      expect(subject.log).to be_empty
+    end
+  end
 
-	describe '#balance' do
-		it 'has no money initially' do
-			expect(subject.balance).to eq 0
-		end
+  describe '#balance' do
+    it 'has no money initially' do
+      expect(subject.balance).to eq 0
+    end
 
-		it 'can be loaded with money' do
-			expect{ subject.add_money(ADD_MONEY) }.to change{ subject.balance }.by ADD_MONEY
-		end
+    it 'can be loaded with money' do
+      expect { subject.add_money(ADD_MONEY) }.to change { subject.balance }.by ADD_MONEY
+    end
 
-		it "has a balance limit of #{Oystercard::BALANCE_LIMIT} Pounds" do
-			subject.add_money(ADD_MONEY)
-			expect{ subject.add_money(ADD_MONEY) }.to raise_error "The balance limit is #{Oystercard::BALANCE_LIMIT} pounds"
-		end
-	end
+    it "has a balance limit of #{Oystercard::BALANCE_LIMIT} Pounds" do
+      subject.add_money(ADD_MONEY)
+      expect { subject.add_money(ADD_MONEY) }.to raise_error "The balance limit is #{Oystercard::BALANCE_LIMIT} pounds"
+    end
+  end
 
-	describe '#touch in' do
-		it 'can touch in when entering the tube' do
-			subject.add_money(ADD_MONEY)
-			subject.touch_in(entry_station)
-			expect(subject.in_journey?).to be true
-		end
+  describe '#touch in' do
+    it 'can touch in when entering the tube' do
+      subject.add_money(ADD_MONEY)
+      subject.touch_in(entry_station)
+      expect(subject.in_journey?).to be true
+    end
 
-		it 'cannot touch in with insufficient funds' do
-			expect { subject.touch_in(entry_station) }.to raise_error "Insufficient Funds!"
-		end
+    it 'cannot touch in with insufficient funds' do
+      expect { subject.touch_in(entry_station) }.to raise_error 'Insufficient Funds!'
+    end
 
+    describe '#touch out' do
+      before do
+        subject.add_money(ADD_MONEY)
+        subject.touch_in(entry_station)
+      end
 
-		describe '#touch out' do
-			before do
-				subject.add_money(ADD_MONEY)
-				subject.touch_in(entry_station)
-			end
+      it 'no longer in a journey after touching out' do
+        subject.touch_out(exit_station)
+        expect(subject.in_journey?).to be false
+      end
 
-			it 'can touch out when leaving the tube' do
-				subject.touch_out(exit_station)
-				expect(subject.in_journey?).to be false
-			end
+      it 'deduces a fare when tapping out' do
+        expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-Oystercard::MINIMUM_FARE)
+      end
 
-			it 'deduces a fare when tapping out' do
-				expect { subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oystercard::MINIMUM_FARE)
-			end
+      it 'should store exit station' do
+        subject.touch_out(exit_station)
+        expect(subject.exit_station).to eq exit_station
+      end
+    end
+  end
 
-			it "should store exit station" do
-				subject.touch_out(exit_station)
-				expect(subject.exit_station).to eq exit_station
-			end
-		end
+  describe 'stores information about travel' do
+    before do
+      subject.add_money(ADD_MONEY)
+      subject.touch_in(entry_station)
+    end
 
+    it 'remembers the station it was last tapped at' do
+      expect(subject.entry_station).to eq entry_station
+    end
 
-	end
+    it 'forgets entry station upon touching out' do
+      expect { subject.touch_out(exit_station) }.to change { subject.entry_station }.to be nil
+    end
 
-	describe 'stores information about travel' do
-		before do
-			subject.add_money(ADD_MONEY)
-			subject.touch_in(entry_station)
-		end
-
-		it 'remembers the station it was last tapped at' do
-			expect(subject.entry_station).to eq entry_station
-		end
-
-		it "forgets entry station upon touching out" do
-			expect { subject.touch_out(exit_station) }.to change{ subject.entry_station }.to be nil
-		end
-
-		it "should store a journey" do
-			subject.touch_out(exit_station)
-			expect(subject.log[-1]).to eq ({ entry_station: entry_station, exit_station: exit_station })
-		end
-	end
+    it 'should store a journey' do
+      subject.touch_out(exit_station)
+      expect(subject.log[-1]).to eq ({ entry_station => exit_station })
+    end
+  end
 end
